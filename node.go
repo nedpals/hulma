ca1f767d4c8a9ace60a49207dca72551de47b97c
Ned Palacios <npdoesmc@gmail.com>
2022-07-30 13:41:15 +0800

    move node types to its own package

--- node.go
+++ node.go
@@ -2,69 +2,28 @@ package main
 
 import (
 	"fmt"
-)
 
-type NodeType string
-
-const (
-	NODE_TYPE_SOURCE    NodeType = "source"
-	NODE_TYPE_DISPLAY   NodeType = "display"
-	NODE_TYPE_STATEMENT NodeType = "statement"
-	NODE_TYPE_INCLUDE   NodeType = "include"
-	NODE_TYPE_BLOCK     NodeType = "block"
-)
-
-type ExpressionNodeType NodeType
-
-const (
-	NODE_TYPE_VARIABLE ExpressionNodeType = "variable"
-	NODE_TYPE_FILTER   ExpressionNodeType = "filter"
-	NODE_TYPE_CONTENT  ExpressionNodeType = "content"
-	NODE_TYPE_FUNCTION ExpressionNodeType = "function"
-)
-
-type StatementNodeType NodeType
-
-const (
-	NODE_TYPE_COND   StatementNodeType = "cond"
-	NODE_TYPE_YIELD  StatementNodeType = "yield"
-	NODE_TYPE_LOOP   StatementNodeType = "loop"
-	NODE_TYPE_ASSIGN StatementNodeType = "assign"
-)
-
-type FunctionNodeType NodeType
-
-const (
-	NODE_TYPE_FUNCTION_PARAMETER FunctionNodeType = "filter_parameter"
-	NODE_TYPE_FUNCTION_ARGUMENT  FunctionNodeType = "filter_argument"
-)
-
-type CondNodeType NodeType
-
-const (
-	NODE_TYPE_COND_EXPR   CondNodeType = "cond_expression"
-	NODE_TYPE_COND_CONSEQ CondNodeType = "cond_consequence"
-	NODE_TYPE_COND_ALTER  CondNodeType = "cond_alternative"
+	types "github.com/nedpals/hulma/node_types"
 )
 
 type Node struct {
-	Type     NodeType
+	Type     types.NodeType
 	Value    string
 	Children []Node
 }
 
 func (node Node) evaluateExpression(tmpl TemplateData) (any, error) {
-	exprType := ExpressionNodeType(node.Type)
+	exprType := types.ExpressionNodeType(node.Type)
 	switch exprType {
-	case NODE_TYPE_CONTENT:
+	case types.NODE_TYPE_CONTENT:
 		return node.Value, nil
-	case NODE_TYPE_VARIABLE:
+	case types.NODE_TYPE_VARIABLE:
 		gotValue, varExists := tmpl.Context.Data[node.Value]
 		if !varExists {
 			return nil, fmt.Errorf("variable `%s` does not exist", node.Value)
 		}
 		return gotValue, nil
-	case NODE_TYPE_FILTER:
+	case types.NODE_TYPE_FILTER:
 		filterFn, filterExists := tmpl.Filters[node.Value]
 		if !filterExists {
 			return nil, fmt.Errorf("filter `%s` does not exist", node.Value)
@@ -76,7 +35,7 @@ func (node Node) evaluateExpression(tmpl TemplateData) (any, error) {
 		}
 
 		return filterFn(evaluatedValue)
-	case NODE_TYPE_FUNCTION:
+	case types.NODE_TYPE_FUNCTION:
 		functionFn, functionExists := tmpl.Functions[node.Value]
 		if !functionExists {
 			filterFn, filterExists := tmpl.Filters[node.Value]
@@ -99,7 +58,7 @@ func (node Node) evaluateExpression(tmpl TemplateData) (any, error) {
 }
 
 func (node Node) collectFunctionArguments(tmpl TemplateData) (any, error) {
-	if node.Type != NodeType(NODE_TYPE_FUNCTION) {
+	if node.Type != types.NodeType(types.NODE_TYPE_FUNCTION) {
 		return nil, fmt.Errorf("node is not a function call")
 	} else if len(node.Children) == 0 {
 		return nil, nil
@@ -111,16 +70,16 @@ func (node Node) collectFunctionArguments(tmpl TemplateData) (any, error) {
 	values := []any{}
 
 	for _, child := range node.Children {
-		fType := FunctionNodeType(child.Type)
+		fType := types.FunctionNodeType(child.Type)
 
 		switch fType {
-		case NODE_TYPE_FUNCTION_PARAMETER:
+		case types.NODE_TYPE_FUNCTION_PARAMETER:
 			if shouldBeMap {
 				shouldBeMap = true
 			}
 			keys = append(keys, child.Value)
 			j++
-		case NODE_TYPE_FUNCTION_ARGUMENT:
+		case types.NODE_TYPE_FUNCTION_ARGUMENT:
 			if len(child.Children) != 0 && len(child.Value) != 0 {
 				return nil, fmt.Errorf("argument value should not be a content or an expression node at the same time")
 			}
@@ -171,19 +130,19 @@ func renderBool(value any) bool {
 }
 
 func (node Node) evaluateStatement(tmpl TemplateData, renderer Renderer) error {
-	stmtType := StatementNodeType(node.Type)
+	stmtType := types.StatementNodeType(node.Type)
 	switch stmtType {
-	case NODE_TYPE_YIELD:
+	case types.NODE_TYPE_YIELD:
 		if gotBlock, blockExists := tmpl.Context.Blocks[node.Value]; blockExists {
 			return renderChildren(gotBlock, tmpl, renderer)
 		} else {
 			return renderChildren(node.Children, tmpl, renderer)
 		}
-	case NODE_TYPE_COND:
-		if len(node.Children) < 2 || CondNodeType(node.Children[0].Type) != NODE_TYPE_COND_EXPR || len(node.Children[0].Children) != 1 {
-			return fmt.Errorf("invalid conditional node")
-		} else if len(node.Children) == 3 && (StatementNodeType(node.Children[2].Type) != NODE_TYPE_COND || CondNodeType(node.Children[2].Type) != NODE_TYPE_COND_ALTER) {
-			return fmt.Errorf("invalid conditional node")
+	case types.NODE_TYPE_COND:
+		if len(node.Children) < 2 || types.CondNodeType(node.Children[0].Type) != types.NODE_TYPE_COND_EXPR || len(node.Children[0].Children) != 1 {
+			return fmt.Errorf("[1] invalid conditional node")
+		} else if len(node.Children) == 3 && (types.StatementNodeType(node.Children[2].Type) != types.NODE_TYPE_COND && types.CondNodeType(node.Children[2].Type) != types.NODE_TYPE_COND_ALTER) {
+			return fmt.Errorf("[2] invalid conditional node")
 		}
 
 		condExpr := node.Children[0]
@@ -197,7 +156,7 @@ func (node Node) evaluateStatement(tmpl TemplateData, renderer Renderer) error {
 			return renderChildren(node.Children[1].Children, tmpl, renderer)
 		} else if len(node.Children) == 3 {
 			// else-if or elif
-			if StatementNodeType(node.Children[2].Type) == NODE_TYPE_COND {
+			if types.StatementNodeType(node.Children[2].Type) == types.NODE_TYPE_COND {
 				return node.Children[2].evaluateStatement(tmpl, renderer)
 			} else {
 				return renderChildren(node.Children[2].Children, tmpl, renderer)
@@ -205,7 +164,7 @@ func (node Node) evaluateStatement(tmpl TemplateData, renderer Renderer) error {
 		} else if len(node.Children) == 4 {
 			return renderChildren(node.Children[3].Children, tmpl, renderer)
 		}
-	case NODE_TYPE_LOOP:
+	case types.NODE_TYPE_LOOP:
 		// for loop dissect
 		// index 0 -
 
@@ -236,7 +195,7 @@ func (node Node) evaluateStatement(tmpl TemplateData, renderer Renderer) error {
 }
 
 func (node Node) scanBlock(parentBlockName string, blocks map[string][]Node) error {
-	if node.Type != NodeType(NODE_TYPE_BLOCK) {
+	if node.Type != types.NodeType(types.NODE_TYPE_BLOCK) {
 		return fmt.Errorf("[scanBlocks] node not a block")
 	} else if node.Value == parentBlockName {
 		return fmt.Errorf("`%s` block should not be recursive", node.Value)
@@ -251,17 +210,17 @@ func (node Node) scanBlock(parentBlockName string, blocks map[string][]Node) err
 
 func (node Node) evaluate(tmpl TemplateData, renderer Renderer) error {
 	switch node.Type {
-	case NODE_TYPE_SOURCE:
+	case types.NODE_TYPE_SOURCE:
 		for _, cn := range node.Children {
 			if err := cn.evaluate(tmpl, renderer); err != nil {
 				return err
 			}
 		}
-	case NodeType(NODE_TYPE_CONTENT):
+	case types.NodeType(types.NODE_TYPE_CONTENT):
 		return renderer.Write(node.Value)
-	case NODE_TYPE_INCLUDE:
+	case types.NODE_TYPE_INCLUDE:
 		return tmpl.Templates.Render(node.Value, tmpl, renderer)
-	case NODE_TYPE_DISPLAY:
+	case types.NODE_TYPE_DISPLAY:
 		if len(node.Children) != 1 {
 			return fmt.Errorf("display node should have exactly one child")
 		}
@@ -270,12 +229,12 @@ func (node Node) evaluate(tmpl TemplateData, renderer Renderer) error {
 			return err
 		}
 		return renderer.Write(gotValue)
-	case NODE_TYPE_STATEMENT:
+	case types.NODE_TYPE_STATEMENT:
 		if len(node.Children) != 1 {
 			return fmt.Errorf("statement node should have exactly one child")
 		}
 		return node.Children[0].evaluateStatement(tmpl, renderer)
-	case NODE_TYPE_BLOCK:
+	case types.NODE_TYPE_BLOCK:
 		return nil
 	default:
 		return fmt.Errorf("[evaluate] unsupported node: %s", node.Type)
---
+++ node_types/node_types.go
@@ -0,0 +1,44 @@
+package nodetypes
+
+type NodeType string
+
+const (
+	NODE_TYPE_SOURCE    NodeType = "source"
+	NODE_TYPE_DISPLAY   NodeType = "display"
+	NODE_TYPE_STATEMENT NodeType = "statement"
+	NODE_TYPE_INCLUDE   NodeType = "include"
+	NODE_TYPE_BLOCK     NodeType = "block"
+)
+
+type ExpressionNodeType NodeType
+
+const (
+	NODE_TYPE_VARIABLE ExpressionNodeType = "variable"
+	NODE_TYPE_FILTER   ExpressionNodeType = "filter"
+	NODE_TYPE_CONTENT  ExpressionNodeType = "content"
+	NODE_TYPE_FUNCTION ExpressionNodeType = "function"
+)
+
+type StatementNodeType NodeType
+
+const (
+	NODE_TYPE_COND   StatementNodeType = "cond"
+	NODE_TYPE_YIELD  StatementNodeType = "yield"
+	NODE_TYPE_LOOP   StatementNodeType = "loop"
+	NODE_TYPE_ASSIGN StatementNodeType = "assign"
+)
+
+type FunctionNodeType NodeType
+
+const (
+	NODE_TYPE_FUNCTION_PARAMETER FunctionNodeType = "filter_parameter"
+	NODE_TYPE_FUNCTION_ARGUMENT  FunctionNodeType = "filter_argument"
+)
+
+type CondNodeType NodeType
+
+const (
+	NODE_TYPE_COND_EXPR   CondNodeType = "cond_expression"
+	NODE_TYPE_COND_CONSEQ CondNodeType = "cond_consequence"
+	NODE_TYPE_COND_ALTER  CondNodeType = "cond_alternative"
+)
